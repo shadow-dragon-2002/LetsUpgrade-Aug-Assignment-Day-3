de92c05f33449eb87e547a0ae7ded216d68a3cbc
nightraven <73079262+shadow-dragon-2002@users.noreply.github.com>
2022-09-02 20:28:31 +0530

    Add files via upload

--- Assignment_day_3.py
+++ Assignment_day_3.py
@@ -1,9 +1,11 @@
 print ("Assignment of Balamurali Krishna S")
+
 print("Printing all Prime Numbers between 1 to 200: ")
-for num in range(1, 200 + 1):
-   if num > 1:
-       for i in range(2, num):
-           if (num % i) == 0:
+
+for num in range(1, 200 + 1):           #go through whole range to look for each number  
+   if num > 1:                          #conditional to check if number greater than 1
+       for i in range(2, num):          
+           if (num % i) == 0:           #conditional to check if number is not a prime number
                break
-       else:
+       else:                            #else statement to print if it is prime number
            print(num)
